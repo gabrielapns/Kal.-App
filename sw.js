5ad92efdf5945c6fce3cb9c338176868e3aaa0d2
Nexora <gabrielmendes25a@gmail.com>
2026-07-19 19:48:55 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
-const CACHE_NAME = 'kal-v1';
-const urlsToCache = [
-  '/Kal_/',
-  '/Kal_/index.html',
-  '/Kal_/icon-192.png',
-  '/Kal_/icon-512.png'
+const CACHE = 'kal-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
